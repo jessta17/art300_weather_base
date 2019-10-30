88aa52d67b1396f3b4263d4f657c2b300b05d172
jessta17 <jessta17@ksu.edu>
2019-10-30 14:07:38 -0500

    update_wednesday

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-const keys = {
-    weatherbit: "bec747b0fb5a4aa68cc53aa94620ecd2"
-    openweathermap: "09796295df446a71ec3c872c565b1838"
+const Keys = {
+    weatherbit: "bec747b0fb5a4aa68cc53aa94620ecd2",
+    openweathermap: "09796295df446a71ec3c872c565b1838",
 }
